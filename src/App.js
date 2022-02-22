dfe448fd4b39bbc7e82300912c6033f2cc3e7ff2
Alex Blair <bclan11@gmail.com>
2022-02-21 23:40:23 -0800

    pie chart working but want to filter out lesser makes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import DataGrid from 'react-data-grid';
 import data from './TableData';
-import { generateColumns, makeCountByAge } from './data-utils';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
-
-// const columns = [
-//   { key: 'id', name: 'ID' },
-//   { key: 'title', name: 'Title' },
-// ];
-
-// const rows = [
-//   { id: 0, title: 'Example' },
-//   { id: 1, title: 'Demo' },
-// ];
+import { generateColumns, makeCountByAge, countMakes } from './data-utils';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryScatter } from 'victory';
+import { VictoryPie } from 'victory';
 
 export default function App() {
+  // console.log(countMakes(data));
   return (
     <>
       <DataGrid columns={generateColumns(data)} rows={data} />;
       <VictoryChart domainPadding={20}>
         <VictoryBar data={makeCountByAge(data)} x="make" y="averageAge" />
       </VictoryChart>
-      ;
+      <VictoryPie
+        data={countMakes(data)}
+        labelPlacement={'parallel'}
+        style={{ labels: { fontSize: 4 } }}
+      />
+      <VictoryScatter data={countMakes(data)} />
     </>
   );
 }
--- src/data-utils.js
+++ src/data-utils.js
@@ -27,3 +27,46 @@ export function makeCountByAge(array) {
     averageAge: entry[1].total / entry[1].count,
   }));
 }
+
+// export function countMakes(array) {
+//   const counter = [];
+//   if (counter.length !== 0) {
+//     array.map((car) => {
+//       counter.map((carCount) => {
+//         if (car.make === carCount.x) {
+//           carCount.y++;
+
+//   } else {
+//       array.map((car) => {
+//         counter.push({
+//           x: car.make,
+//           y: 1,
+//         });
+//       })
+//       }
+//     }
+//     console.log(counter);
+//     return counter;
+//   });
+// }
+
+export function countMakes(array) {
+  const counter = [];
+  array.map((car) => {
+    if (counter.length === 0) {
+      array.map((singleCar) => {
+        counter.push({
+          x: singleCar.make,
+          y: 1,
+        });
+      });
+    } else {
+      array.map((singleCar) => {
+        counter.map((carCount) => {
+          singleCar.make === carCount.x && carCount.y++;
+        });
+      });
+    }
+  });
+  return counter;
+}
