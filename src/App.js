2e879d026c7da0353d14a65c6136b29f543aa088
Alex Blair <bclan11@gmail.com>
2022-02-22 18:04:48 -0800

    PIE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import DataGrid from 'react-data-grid';
 import data from './TableData';
 import { generateColumns, makeCountByAge, countMakes } from './data-utils';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryScatter } from 'victory';
+import { VictoryBar, VictoryChart, VictoryScatter } from 'victory';
 import { VictoryPie } from 'victory';
 
 export default function App() {
   // console.log(countMakes(data));
   return (
     <>
+      Test
       <DataGrid columns={generateColumns(data)} rows={data} />;
       <VictoryChart domainPadding={20}>
         <VictoryBar data={makeCountByAge(data)} x="make" y="averageAge" />
--- src/data-utils.js
+++ src/data-utils.js
@@ -28,45 +28,31 @@ export function makeCountByAge(array) {
   }));
 }
 
-// export function countMakes(array) {
-//   const counter = [];
-//   if (counter.length !== 0) {
-//     array.map((car) => {
-//       counter.map((carCount) => {
-//         if (car.make === carCount.x) {
-//           carCount.y++;
-
-//   } else {
-//       array.map((car) => {
-//         counter.push({
-//           x: car.make,
-//           y: 1,
-//         });
-//       })
-//       }
-//     }
-//     console.log(counter);
-//     return counter;
-//   });
-// }
-
 export function countMakes(array) {
-  const counter = [];
-  array.map((car) => {
-    if (counter.length === 0) {
-      array.map((singleCar) => {
-        counter.push({
-          x: singleCar.make,
-          y: 1,
-        });
-      });
+  const desiredMakes = array.filter(
+    (item) =>
+      item.make === 'Honda' ||
+      item.make === 'Toyota' ||
+      item.make === 'Ford' ||
+      item.make === 'Audi' ||
+      item.make === 'Dodge' ||
+      item.make === 'Subaru'
+  );
+  const hashMap = desiredMakes.reduce((acc, curr) => {
+    if (acc[curr.make]) {
+      acc[curr.make]++;
     } else {
-      array.map((singleCar) => {
-        counter.map((carCount) => {
-          singleCar.make === carCount.x && carCount.y++;
-        });
-      });
+      acc[curr.make] = 1;
     }
+    return acc;
+  }, {});
+  const refactorObject = Object.entries(hashMap);
+  const objectMap = refactorObject.map((object) => {
+    const carObj = {
+      x: object[0],
+      y: object[1],
+    };
+    return carObj;
   });
-  return counter;
+  return objectMap;
 }
